8474eab7df9e9dbf1bae5c4d46b759d72501101e
soabel <alfredo.benaute@gmail.com>
2020-12-10 12:12:12 -0500

    add initial projects

---
+++ sincrono/src/test/java/com/bootcamp/example/sincrono/SincronoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.example.sincrono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SincronoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
